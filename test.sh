1d01298e39720399da19fa2dfdf87bd0c271b84e
Linuxcode1 <64568214+Linuxcode1@users.noreply.github.com>
2022-10-22 13:14:18 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-
-##################################################################
-###       Beta-PRO-SKIN-FHD      BY Bahaa (LinuXcode)          ###
-###       Beta-PRO-SKIN-PLUGIN   Coded BY Bahaa (LinuXcode)    ###
-##################################################################
-
-Skin_BETAPRO _version=3
-Plugin_BETA-PRO_version=1
